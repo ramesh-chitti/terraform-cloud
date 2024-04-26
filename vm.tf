2ba4e8840700cf9ebc2dcc861e9e011a4e4e898f
Ramesh chitti <ramesh.chitti@gmail.com>
2024-04-26 04:32:10 +0000

    Added VM2 removed

--- vm.tf
+++ vm.tf
@@ -1,13 +1,13 @@
 resource "azurerm_linux_virtual_machine" "example" {
-  count = 2
+  #count = 2
 
-  name                = "mylinuxvm.count.index"
+  name                = "mylinuxvm"
   location            = azurerm_resource_group.myrg.location
   resource_group_name = azurerm_resource_group.myrg.name
   size                = "Standard_DS1_v2"
   admin_username      = "adminuser"
   network_interface_ids = [
-    azurerm_network_interface.myvmnic.id.count.index
+    azurerm_network_interface.myvmnic.id
   ]
   admin_ssh_key {
     username   = "adminuser"
