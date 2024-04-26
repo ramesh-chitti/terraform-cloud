febeaa819b1b297d7a7d052c82d09d259fd56b28
Ramesh chitti <ramesh.chitti@gmail.com>
2024-04-26 03:40:36 +0000

    terraform-azure.pub key is fixed

--- vm.tf
+++ vm.tf
@@ -12,7 +12,7 @@ resource "azurerm_linux_virtual_machine" "example" {
 
   admin_ssh_key {
     username   = "adminuser"
-    public_key = file("${path.module}/ssh-keys/terrafrom-azure.pub")
+    public_key = file(~/.ssh-keys/terrafrom-azure.pub")
   }
 
   os_disk {
